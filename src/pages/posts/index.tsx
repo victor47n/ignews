02ad6ee23bfe034dbb8c23583bc402193744c7fc
victor47dev <victor47n@gmail.com>
2022-01-05 22:43:23 -0300

    Estilizado pagina de posts

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,32 @@
+import Head from 'next/head';
+import styles from './styles.module.scss'
+
+export default function Posts() {
+    return (
+        <>
+            <Head>
+                <title>Posts | Ignews</title>
+            </Head>
+
+            <main className={styles.container}>
+                <div className={styles.posts}>
+                    <a href="#">
+                        <time>05 de janeiro de 2022</time>
+                        <strong>Dark Mode com CSS — mudando a aparência do Blog de maneira simples e rápida</strong>
+                        <p>Umas das funcionalidades que está na moda em Blogs e Sites é o Dark Mode. Devs, em sua maioria, curtem bastante utilizar temas escuros, tanto na IDE quanto em outros apps.</p>
+                    </a>
+                    <a href="#">
+                        <time>05 de janeiro de 2022</time>
+                        <strong>Dark Mode com CSS — mudando a aparência do Blog de maneira simples e rápida</strong>
+                        <p>Umas das funcionalidades que está na moda em Blogs e Sites é o Dark Mode. Devs, em sua maioria, curtem bastante utilizar temas escuros, tanto na IDE quanto em outros apps.</p>
+                    </a>
+                    <a href="#">
+                        <time>05 de janeiro de 2022</time>
+                        <strong>Dark Mode com CSS — mudando a aparência do Blog de maneira simples e rápida</strong>
+                        <p>Umas das funcionalidades que está na moda em Blogs e Sites é o Dark Mode. Devs, em sua maioria, curtem bastante utilizar temas escuros, tanto na IDE quanto em outros apps.</p>
+                    </a>
+                </div>
+            </main>
+        </>
+    );
+}
