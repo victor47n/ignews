6b6c3025624e27a13937a1bc5da6918da8682cde
victor47dev <victor47n@gmail.com>
2023-02-16 21:07:05 -0300

    Finalização do projeto

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,52 +1,74 @@
 import { GetStaticProps } from 'next'
+import Link from 'next/link'
 import Head from 'next/head'
 import { Client } from '../../utils/prismicHelpers'
 import Prismic from '@prismicio/client'
 import styles from './styles.module.scss'
+import { RichText } from 'prismic-dom'
 
-export default function Posts() {
-    return (
-        <>
-            <Head>
-                <title>Posts | Ignews</title>
-            </Head>
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
 
-            <main className={styles.container}>
-                <div className={styles.posts}>
-                    <a href="#">
-                        <time>05 de janeiro de 2022</time>
-                        <strong>Dark Mode com CSS — mudando a aparência do Blog de maneira simples e rápida</strong>
-                        <p>Umas das funcionalidades que está na moda em Blogs e Sites é o Dark Mode. Devs, em sua maioria, curtem bastante utilizar temas escuros, tanto na IDE quanto em outros apps.</p>
-                    </a>
-                    <a href="#">
-                        <time>05 de janeiro de 2022</time>
-                        <strong>Dark Mode com CSS — mudando a aparência do Blog de maneira simples e rápida</strong>
-                        <p>Umas das funcionalidades que está na moda em Blogs e Sites é o Dark Mode. Devs, em sua maioria, curtem bastante utilizar temas escuros, tanto na IDE quanto em outros apps.</p>
-                    </a>
-                    <a href="#">
-                        <time>05 de janeiro de 2022</time>
-                        <strong>Dark Mode com CSS — mudando a aparência do Blog de maneira simples e rápida</strong>
-                        <p>Umas das funcionalidades que está na moda em Blogs e Sites é o Dark Mode. Devs, em sua maioria, curtem bastante utilizar temas escuros, tanto na IDE quanto em outros apps.</p>
-                    </a>
-                </div>
-            </main>
-        </>
-    );
+interface PostsProps {
+  posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
+  console.log(posts)
+
+  return (
+    <>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          {
+            posts.map(post => (
+              <Link key={post.slug} href={`/posts/${post.slug}`}>
+                <a>
+                  <time>{post.updatedAt}</time>
+                  <strong>{post.title}</strong>
+                  <p>{post.excerpt}</p>
+                </a>
+              </Link>
+            ))
+          }
+        </div>
+      </main>
+    </>
+  );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-    const prismic = Client();
+  const prismic = Client();
 
-    const response = await prismic.query(
-        [Prismic.Predicates.at("document.type", "post")],
-        {
-            fetch: ['post.title', 'post.content'],
-            pageSize: 100,
-        })
+  const response = await prismic.query<any>(
+    [Prismic.Predicates.at("document.type", "post")],
+    {
+      fetch: ['post.title', 'post.content'],
+      pageSize: 100,
+    })
 
-    console.log(response);
-
+  const posts = response.results.map(post => {
     return {
-        props: {}
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
     }
+  })
+
+  return {
+    props: { posts }
+  }
 }
